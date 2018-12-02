ee54022e7c1de5fa14ab52c7623d427f524457b7
Alfonso David Navarro Medina <alfonsonavarromedina@gmail.com>
2018-12-02 16:49:06 -0400

    Mejorados los urls del proyecto

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import url, include
-from django.urls import path
-from marvel import views
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls), 
-    url(r'^$',views.homepage),
-    url(r'^battle/$',views.battleGroup)        
+    path(r'admin/', admin.site.urls), 
+    path(r'', include('marvel.urls'))        
 ]
