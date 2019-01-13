5b7d368800e1a478e62840481cfff61b61c866d1
jkdky05 <41238847+jkdky05@users.noreply.github.com>
2019-01-12 21:00:33 -0400

    Función para buscar todas las batallas de un evento

--- marvel/templatetags/consultas_Evento.py
+++ marvel/templatetags/consultas_Evento.py
@@ -17,3 +17,25 @@ def convertirEnJson_even(listaEventos):
         }  
         listaRespuesta.append(jsonRespuesta)
     return json.dumps(listaRespuesta)
+
+//Busca todas las batallas de un evento
+def searchBattles(fechaIni):
+    try:
+        datEven = Even.objects.get(fech_in=fecha)
+    except:
+        print('El evento no existe')
+        return(-1)
+    try:
+        dat = Combat.objects.all()
+    except:
+        print('No hay batallas en el evento')
+        return(-1)
+    for i in dat:
+        if (i.fech >= fechaIni) and (i.fech <= datEven.fech_fin):
+            try:
+                data.append = i
+            except:
+                 print('Hubo un error al agregar')
+    for e in data:
+        print(e)
+    return(data)
